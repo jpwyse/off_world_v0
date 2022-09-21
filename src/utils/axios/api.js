61fdac55d4713c254481752918404b6f2b9bf585
James Wyse <jpwyse@gmail.com>
2022-09-21 19:01:47 -0400

    deploy axios update

--- src/utils/axios/api.js
+++ src/utils/axios/api.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+console.log(window.location.origin);
+console.log(axiosBaseURL);
+
+if (window.location.origin === "http://localhost:8000") {
+  const axiosBaseURL = "http://127.0.0.1:8000"; // development address
+} else {
+  const axiosBaseURL = window.location.origin; // production address
+}
+
+console.log(axiosBaseURL);
+
 export default axios.create({
-	baseURL: "http://localhost:8000/",
+	baseURL: axiosBaseURL,
 });
