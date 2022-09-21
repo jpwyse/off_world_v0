985463709419b659e57dcc45eacb679ad3090836
James Wyse <jpwyse@gmail.com>
2022-09-21 16:27:21 -0400

    deploy mime update

--- offworld_v0/urls.py
+++ offworld_v0/urls.py
@@ -23,9 +23,12 @@ from . api import api
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
-] + static('/media/', document_root=settings.MEDIA_ROOT)
+]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                        document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+# This is will cath the routes in the frontend and 404 errors
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
