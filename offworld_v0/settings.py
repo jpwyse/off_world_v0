70dd6fc701bb9f48673c26bccf841a59c231362c
James Wyse <jpwyse@gmail.com>
2022-09-20 21:06:12 -0400

    deploy update 3

--- offworld_v0/settings.py
+++ offworld_v0/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
 
 ROOT_URLCONF = 'offworld_v0.urls'
