4b4e1cdbf7569e4bec58472e5cbed11d9e5fd807
James Wyse <jpwyse@gmail.com>
2022-09-20 20:02:28 -0400

    first commit 2nd udpate

--- offworld_v0/settings.py
+++ offworld_v0/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-56h1png1g6(y0i!2=c$ev3m7p4&8^*%8tk7mfeyt^g$zt7nx=w'
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
