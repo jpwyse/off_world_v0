ac1731f5eeaf21c1ff8be380ca5b40605a539136
James Wyse <jpwyse@gmail.com>
2022-09-22 17:54:03 -0400

    settings static update

--- offworld_v0/settings.py
+++ offworld_v0/settings.py
@@ -150,7 +150,7 @@ AUTH_USER_MODEL = 'Users.User'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'build/static/')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
