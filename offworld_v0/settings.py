ed50d1bd923e3a394c0274c43d8cff6d4e1ac47b
James Wyse <jpwyse@gmail.com>
2022-09-21 20:01:27 -0400

    deploy mimetype update

--- offworld_v0/settings.py
+++ offworld_v0/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import django_heroku
 import dj_database_url
+import mimetypes
+
+mimetypes.add_type("text/javascript", ".js", True)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
