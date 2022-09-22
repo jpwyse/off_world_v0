b479aa2bfd4798acde6e67089c9ac71a9bb87fb3
James Wyse <jpwyse@gmail.com>
2022-09-22 18:03:03 -0400

    media_root settings update

--- offworld_v0/settings.py
+++ offworld_v0/settings.py
@@ -150,12 +150,12 @@ AUTH_USER_MODEL = 'Users.User'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static/')
+    os.path.join(BASE_DIR, 'build/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 try:
